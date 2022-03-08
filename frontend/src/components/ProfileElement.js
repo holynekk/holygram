a69dcc576b3ee5f18707d862d9243fb500b83d47
holynekk <e238109@metu.edu.tr>
2022-03-08 23:07:03 +0300

    search re-styled

--- frontend/src/components/ProfileElement.js
+++ frontend/src/components/ProfileElement.js
@@ -5,12 +5,11 @@ function ProfileElement({user_name, first_name, last_name, profile_picture, foll
     const navigate = useNavigate();
     return (
         <div className='profile-element-wrapper'>
-            1111
-            <img src={"#"}></img>
-            <div profile-element-info>
-                <p onClick={()=>navigate('/profile/' + user_name)}>{user_name}</p>
+            <img src={"https://picsum.photos/80/80"}></img>
+            <div className='profile-element-info' >
+                <p style={{fontWeight: "900"}} onClick={()=>navigate('/profile/' + user_name)}>{user_name}</p>
                 <p>{first_name + " " + last_name}</p>
-                <p>{followers} Followers</p>
+                <p> Followers <bold>{0}</bold></p>
             </div>
         </div>
     );
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -24,7 +24,11 @@ function Search() {
                     onChange={handleSearchText}
                     placeholder="Search"
                 />
-                <button onClick={searchUsers}>Search</button>
+                <button onClick={searchUsers}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+                </svg>
+                </button>
             </div>
             <div className='search-results'>
                 {
@@ -33,7 +37,6 @@ function Search() {
                         <div className='search-results-wrapper'>
                             {
                                 searchResults.map((item, itemIndex)=>{
-                                    console.log(searchResults);
                                     return <ProfileElement {...item} key={itemIndex} />
                                 })
                             }
